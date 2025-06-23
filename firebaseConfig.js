29c620ddb0db7c202223265254d08cf22f33dd28
snowy <trigger.snowii@gmail.com>
2025-06-24 00:18:49 +0900

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyD89VKWK59CHZg3UfQggJO4zgtS9tW8gmo",
+    authDomain: "weot-chat.firebaseapp.com",
+    projectId: "weot-chat",
+    storageBucket: "weot-chat.firebasestorage.app",
+    messagingSenderId: "963922145012",
+    appId: "1:963922145012:web:c7a27a71b880fb0ddaf8a1",
+    measurementId: "G-BF905101QQ"
+  };
+
+  
